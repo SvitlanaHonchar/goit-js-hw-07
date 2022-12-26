68beafea795fea181a01ae140d55061e4aeeda85
SvitlanaHonchar <snidlty@gmail.com>
2022-12-26 22:03:54 +0200

    task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,9 +2,9 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 console.log(galleryItems);
 
-// ---створення розмітки
 const galleryWholeEl = document.querySelector(".gallery");
 
+// ---створення розмітки
 function createGalleryEl({ preview, original, description } = {}) {
   return `<div class="gallery__item">
   <a class="gallery__link" href='${original}'>
@@ -25,13 +25,36 @@ const creatingGallery = galleryItems
 galleryWholeEl.insertAdjacentHTML("beforeend", creatingGallery);
 // ---/створення розмітки
 
+// ---модальне вікно
+const origImgEl = basicLightbox.create(`
+    <div class="modal">
+        <img src='https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg' width="800" height="600">
+    </div>
+`);
+// ---/модальне вікно
+
 // ---створення події
 
 function onGalleryImgClick(e) {
   e.preventDefault();
   const { target } = e;
+  if (target.nodeName !== "IMG") {
+    return;
+  }
+
+  // return target.dataset.source;
+  origImgEl.show();
+  const modalImgEl = document.querySelector(".modal img");
+  modalImgEl.src = target.dataset.source;
+}
+
+function onEscKeyDown(e) {
+  if (e.code === "Escape") {
+    origImgEl.close();
+  }
 }
 
 galleryWholeEl.addEventListener("click", onGalleryImgClick);
+document.addEventListener("keydown", onEscKeyDown);
 
 // ---/створення події
