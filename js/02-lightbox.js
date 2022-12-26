595382575817c90912a87d274e5d82a025a9bbc3
SvitlanaHonchar <snidlty@gmail.com>
2022-12-26 23:49:48 +0200

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryWholeEl = document.querySelector(".gallery");
+
+// ---створення розмітки
+function createGalleryEl({ preview, original, description } = {}) {
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}
+
+const creatingGallery = galleryItems
+  .map((galleryItem) => createGalleryEl(galleryItem))
+  .join("");
+
+galleryWholeEl.insertAdjacentHTML("beforeend", creatingGallery);
+// ---/створення розмітки
+
+// ---створення події
+
+function onGalleryImgClick(e) {
+  e.preventDefault();
+  const { target } = e;
+  if (target.nodeName !== "IMG") {
+    return;
+  }
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+
+  //   console.log(target.alt);
+}
+
+galleryWholeEl.addEventListener("click", onGalleryImgClick);
+
+// ---/створення події
